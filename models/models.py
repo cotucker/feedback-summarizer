4c81e5a3ee79e27cace23d5c81edf1b839ad2f9f
cotucker <klimilya005@gmail.com>
2025-10-26 14:45:48 +0300

    Add data models for API responses

---
+++ models/models.py
@@ -0,0 +1,27 @@
+import enum
+from pydantic import BaseModel
+
+class Sentiment(enum.Enum):
+    POSITIVE = "Positive"
+    NEGATIVE = "Negative"
+    NEUTRAL = "Neutral"
+
+class FeedbackResponse(BaseModel):
+    original_feedback_text: str
+    response: str
+    score: int
+
+class SentimentResponse(BaseModel):
+    text: str
+    topic: str
+    sentiment: Sentiment
+
+class TopicSummary(BaseModel):
+    topic: str
+    summary: str
+
+class TotalSummary(BaseModel):
+    summary: str
+
+class Separator(BaseModel):
+    separator: str
