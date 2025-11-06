7d9683f9ad46580a166d0e78eff7dbecab3e95fc
cotucker <klimilya005@gmail.com>
2025-11-06 17:53:34 +0300

    Update topic names and LLM model
    Updated topic names to be more relevant. Also, updated the LLM model to
    gemini-flash-latest for improved performance.

--- services/llm_service.py
+++ services/llm_service.py
@@ -55,7 +55,7 @@ def generate_topics_description(cluster_names: list[str]) -> list[ClusterDescrip
 
 def generate_cluster_name(cluster_topics: str) -> str:
     response = client.models.generate_content(
-        model='gemini-2.5-flash',
+        model='gemini-flash-latest',
         contents=[
             types.Content(
                 role="user",
